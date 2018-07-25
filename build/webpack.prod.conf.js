d0e6bf0ba93fa81b0e574fd215afe605f989b9a2
Richard Feldman <richard.t.feldman@gmail.com>
2018-07-24 21:37:14 -0400

    Disable code splitting

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -28,6 +28,9 @@ var webpackConfig = merge(baseWebpackConfig, {
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
   plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    }),
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
